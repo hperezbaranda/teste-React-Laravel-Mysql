d0da9e8ee68695f8ec3a005bd76f2ae7ee4319f1
Hector Perez Baranda <ticolotico@gmaol.com>
2019-06-28 20:13:54 -0300

    :sparkles: Criando as migracoes

---
+++ database/migrations/2019_06_28_211452_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->longText('descriao');
+            $table->string('unidade');
+            $table->integer('quantidade');
+            $table->double('preco');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
