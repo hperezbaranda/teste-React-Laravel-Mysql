c21ec4ff4e7fbc1949a67db3275da8bd0739df72
Hector Perez Baranda <ticolotico@gmaol.com>
2019-06-30 12:26:54 -0300

    :sparkles: Separando em componentes

---
+++ resources/js/components/Clientes.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import TableComponent from './TableComponent';
+
+
+export default class Clientes extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clientes: []
+        }
+        this.deleteClick = this.deleteClick.bind(this);
+    }
+    componentWillMount() {
+        Axios.get('/api/clientes').then(Response => {
+            this.setState({
+                clientes: Response.data
+            });
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row center">
+                    <Paper className="root">
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Nome</TableCell>
+                                    <TableCell align="center">Sobrenome</TableCell>
+                                    <TableCell align="center">CPF</TableCell>
+                                    <TableCell align="center">Email</TableCell>
+                                    <TableCell align="center">Data Nascimento</TableCell>
+                                    <TableCell align="center">Acao</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.clientes.map(row => (
+                                    <TableRow key={row.id}>
+                                        <TableCell component="th" scope="row">
+                                            {row.nome}
+                                        </TableCell>
+                                        <TableCell align="center">{row.sobrenome}</TableCell>
+                                        <TableCell align="center">{row.cpf}</TableCell>
+                                        <TableCell align="center">{row.email}</TableCell>
+                                        <TableCell align="center">{row.data_nascimento}</TableCell>
+                                        <TableCell align="center"> <IconButton aria-label="Delete" onClick={() => this.deleteClick(row.id)}>
+                                            <DeleteIcon />
+                                        </IconButton></TableCell>
+                                    </TableRow>
+                                ))}
+
+                            </TableBody>
+                            {/* <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                colSpan={3}
+                                count={this.props.count}
+                                rowsPerPage={rowsPerPage}
+                                page={0}
+                                // SelectProps={{
+                                //     inputProps: { 'aria-label': 'Rows per page' },
+                                //     native: true,
+                                // }}
+                                onChangePage={this.handleChangePage}
+                                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                            // ActionsComponent={TablePaginationActions}
+                            />
+                        </TableRow>
+                    </TableFooter> */}
+                        </Table>
+                    </Paper>
+                </div>
+                <div className="row">
+                    
+                </div>
+            </div >
+        );
+    }
+
+    deleteClick(id) {
+        Axios.delete(`/api/clientes/${id}`).then(Response => {
+            let filteredArray = this.state.clientes.filter(item => item.id !== id)
+            this.setState({ clientes: filteredArray });
+        });
+    }
+}
+
+if (document.getElementById('clientes')) {
+    ReactDOM.render(<Clientes />, document.getElementById('clientes'));
+}
---
+++ resources/js/components/Produtos.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import TableComponent from './TableComponent';
+
+
+export default class Produtos extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            produtos:[]
+        }
+        this.deleteClick=this.deleteClick.bind(this);
+    }
+    componentWillMount() {
+
+        Axios.get('/api/produtos').then(Response => {
+            this.setState({
+                produtos: Response.data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Paper className="root">
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Nome</TableCell>
+                            <TableCell align="center">Descrisao</TableCell>
+                            <TableCell align="center">Unidade</TableCell>
+                            <TableCell align="center">Quantidade</TableCell>
+                            <TableCell align="center">Preco</TableCell>
+                            <TableCell align="center">Acao</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.produtos.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell component="th" scope="row">
+                                    {row.nome}
+                                </TableCell>
+                                <TableCell align="center">{row.descricao}</TableCell>
+                                <TableCell align="center">{row.unidade}</TableCell>
+                                <TableCell align="center">{row.quantidade}</TableCell>
+                                <TableCell align="center">R$ {row.preco}</TableCell>
+                                <TableCell align="center"> <IconButton aria-label="Delete" onClick={() => this.deleteClick(row.id)}>
+                                    <DeleteIcon />
+                                </IconButton></TableCell>
+                            </TableRow>
+                        ))}
+
+                    </TableBody>
+                    {/* <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                colSpan={3}
+                                count={this.props.count}
+                                rowsPerPage={rowsPerPage}
+                                page={0}
+                                // SelectProps={{
+                                //     inputProps: { 'aria-label': 'Rows per page' },
+                                //     native: true,
+                                // }}
+                                onChangePage={this.handleChangePage}
+                                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                            // ActionsComponent={TablePaginationActions}
+                            />
+                        </TableRow>
+                    </TableFooter> */}
+                </Table>
+            </Paper>
+            </div >
+        );
+    }
+
+    deleteClick(id) {
+        Axios.delete(`/api/produtos/${id}`).then(Response =>{
+            let filteredArray = this.state.produtos.filter(item => item.id !== id)
+            this.setState({produtos: filteredArray});
+        });
+    }
+}
+
+if (document.getElementById('produtos')) {
+    ReactDOM.render(<Produtos />, document.getElementById('produtos'));
+}
